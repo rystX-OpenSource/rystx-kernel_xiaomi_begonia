89f30c43521f2a68b28f1ae980c9fda284c6ef96
Toshiaki Makita <toshiaki.makita1@gmail.com>
2019-06-13 18:39:58 +0900

    UPSTREAM: xdp: Add tracepoint for bulk XDP_TX
    This is introduced for admins to check what is happening on XDP_TX when
    bulk XDP_TX is in use, which will be first introduced in veth in next
    commit.
    
    v3:
    - Add act field to be in line with other XDP tracepoints.
    
    Change-Id: I0a8904ab72819428a34ad0225c2b094c15efb148
    Signed-off-by: Toshiaki Makita <toshiaki.makita1@gmail.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -2132,9 +2132,4 @@ EXPORT_SYMBOL(bpf_stats_enabled_key);
 #include <linux/bpf_trace.h>
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(xdp_exception);
-
-/* These are only used within the BPF_SYSCALL code */
-#ifdef CONFIG_BPF_SYSCALL
-EXPORT_TRACEPOINT_SYMBOL_GPL(bpf_prog_get_type);
-EXPORT_TRACEPOINT_SYMBOL_GPL(bpf_prog_put_rcu);
-#endif
+EXPORT_TRACEPOINT_SYMBOL_GPL(xdp_bulk_tx);
