bbccb110d5a4a81af51a7b0674992cbc9ed82b8a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-13 21:25:49 +0800

    BRICKPORT: sys_umount: refactor to extract path_umount
    this is a brickport.
    
    we have everything needed anyway.
    
    adapted from the following:
    `fs: add ksys_mount() helper; remove in-kernel calls to sys_mount()`
    - https://github.com/torvalds/linux/commit/312db1aa1dc7bff133d95c92efcc5e42b57cefa6
    `fs: refactor ksys_umount`
    - https://github.com/torvalds/linux/commit/41525f56e2564c2feff4fb2824823900efb3a39f
    `init: add an init_umount helper`
    - https://github.com/torvalds/linux/commit/09267defa36aaff6ff829bd2fc8b043ec151cc3e
    `fs: fix a struct path leak in path_umount`
    - https://github.com/torvalds/linux/commit/25ccd24ffd9119c452d711efa2604a7a0c35956e
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1730,50 +1730,41 @@ static inline bool may_mandlock(void)
 }
 #endif
 
-/**
- * path_mounted - check whether path is mounted
- * @path: path to check
- *
- * Determine whether @path refers to the root of a mount.
- *
- * Return: true if @path is the root of a mount, false if not.
- */
-static inline bool path_mounted(const struct path *path)
-{
-	return path->mnt->mnt_root == path->dentry;
-}
-
-static int can_umount(const struct path *path, int flags)
-{
-	struct mount *mnt = real_mount(path->mnt);
-
-	if (!may_mount())
-		return -EPERM;
-	if (!path_mounted(path))
-		return -EINVAL;
-	if (!check_mnt(mnt))
-		return -EINVAL;
-	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		return -EINVAL;
-	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		return -EPERM;
-	return 0;
-}
-
 // caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
-	struct mount *mnt = real_mount(path->mnt);
-	int ret;
+	struct mount *mnt;
+	int retval;
+	bool user_request = !(current->flags & PF_KTHREAD);
 
-	ret = can_umount(path, flags);
-	if (!ret)
-		ret = do_umount(mnt, flags);
+	if (!may_mount())
+		return -EPERM;
 
+	mnt = real_mount(path->mnt);
+	retval = -EINVAL;
+	if (path->dentry != path->mnt->mnt_root)
+		goto dput_and_out;
+	if (!check_mnt(mnt))
+		goto dput_and_out;
+	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
+		goto dput_and_out;
+	retval = -EPERM;
+	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
+		goto dput_and_out;
+
+	retval = do_umount(mnt, flags);
+dput_and_out:
 	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
 	dput(path->dentry);
+	if (user_request && (!retval || (flags & MNT_FORCE))) {
+		/* filesystem needs to handle unclosed namespaces */
+		if (mnt->mnt.mnt_sb->s_op->umount_end)
+			mnt->mnt.mnt_sb->s_op->umount_end(mnt->mnt.mnt_sb,
+					flags);
+	}
 	mntput_no_expire(mnt);
-	return ret;
+out:
+	return retval;
 }
 
 /*
@@ -1787,41 +1778,20 @@ int path_umount(struct path *path, int flags)
 SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 {
 	struct path path;
-	struct mount *mnt;
-	int retval;
 	int lookup_flags = 0;
+	int retval;
 
 	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
 		return -EINVAL;
-
-	if (!may_mount())
-		return -EPERM;
 
 	if (!(flags & UMOUNT_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
 
 	retval = user_path_mountpoint_at(AT_FDCWD, name, lookup_flags, &path);
 	if (retval)
-		goto out;
-	mnt = real_mount(path.mnt);
-	retval = -EINVAL;
-	if (path.dentry != path.mnt->mnt_root)
-		goto dput_and_out;
-	if (!check_mnt(mnt))
-		goto dput_and_out;
-	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		goto dput_and_out;
-	retval = -EPERM;
-	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		goto dput_and_out;
-
-	retval = do_umount(mnt, flags);
-dput_and_out:
-	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
-	dput(path.dentry);
-	mntput_no_expire(mnt);
-out:
-	return retval;
+		return retval;
+	
+	return path_umount(&path, flags);
 }
 
 #ifdef __ARCH_WANT_SYS_OLDUMOUNT
