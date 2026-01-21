9cd122d219bc5c556de608cadf749fe11eb441d1
iDeadXS <datarafi43@gmail.com>
2026-01-21 05:54:09 +0000

    perf changes

--- drivers/misc/mediatek/base/power/mt6785/mtk_gpufreq_core.c
+++ drivers/misc/mediatek/base/power/mt6785/mtk_gpufreq_core.c
@@ -3041,7 +3041,7 @@ static void __mt_gpufreq_setup_opp_table(
 		g_segment_max_opp_idx = 0;
 	else if (g_segment_id == MT6785T_SEGMENT)
 		// g_segment_max_opp_idx = 15;
-		g_segment_max_opp_idx = 7; // OC 871MHz
+		g_segment_max_opp_idx = 5; // OC
 	else if (g_segment_id == MT6785_SEGMENT)
 		g_segment_max_opp_idx = 21;
 	else if (g_segment_id == MT6783_SEGMENT)
